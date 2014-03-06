ba2bfa8bffff2a548377d6784013e8f5e1c1bbf9
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-03-06 11:02:25 +0100

    Changes to embed templates into js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp		= require("gulp");
 var concat		= require("gulp-concat");
+var ngHtml2Js	= require("gulp-ng-html2js");
+var uglify		= require("gulp-uglify");
 var livereload	= require("gulp-livereload");
 var tinyLr		= require("tiny-lr");
 var static		= require("node-static");
@@ -7,7 +9,7 @@ var http		= require("http");
 
 var lrServer = tinyLr();
 var dvServer = http.createServer(function (req, res) {
-	var stServer = new static.Server("./demo", {cache: false});
+	var stServer = new static.Server(".", {cache: false});
 	req.on("end", function () {
 		stServer.serve(req, res);
 	});
@@ -16,13 +18,26 @@ var dvServer = http.createServer(function (req, res) {
 
 gulp.task("scripts", function () {
 	gulp.src("src/**/*.js")
-		.pipe(concat("ps-ui.js"))
-		.pipe(gulp.dest("."))
-		.pipe(livereload(lrServer));
+		.pipe(concat("ps-ui.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/"));
 });
 
 gulp.task("templates", function () {
-	gulp.src("ps-ui.css")
+	gulp.src("template/**/*.html")
+		.pipe(ngHtml2Js({
+			moduleName: "ps-ui.ps-template",
+			prefix: "template/"
+		}))
+		.pipe(concat("ps-template.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/"));
+});
+
+gulp.task("final", function () {
+	gulp.src(["dist/ps-ui.min.js", "dist/ps-template.js"])
+		.pipe(concat("ps-ui-tpls.min.js"))
+		.pipe(gulp.dest("dist/"))
 		.pipe(livereload(lrServer));
 });
 
@@ -31,4 +46,6 @@ gulp.task("default", function () {
 	lrServer.listen(35729);
 	gulp.watch("src/**/*.js", ["scripts"]);
 	gulp.watch("template/**/*.html", ["templates"]);
+	gulp.watch("dist/ps-ui.min.js", ["final"]);
+	gulp.watch("dist/ps-template.min.js", ["final"]);
 });
