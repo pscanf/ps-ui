d1e6fc9c71daf678b24bcbe72e6591e3d2a79d7f
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-03-06 11:26:47 +0100

    Fixed path error in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -34,16 +34,16 @@ gulp.task("scripts", function () {
 gulp.task("templates", function () {
 	gulp.src("template/**/*.html")
 		.pipe(ngHtml2Js({
-			moduleName: "ps-ui.ps-template",
+			moduleName: "ps-ui.ps-template-cache",
 			prefix: "template/"
 		}))
-		.pipe(concat("ps-template.min.js"))
+		.pipe(concat("ps-template-cache.min.js"))
 		.pipe(uglify())
 		.pipe(gulp.dest("dist/"));
 });
 
 gulp.task("final", function () {
-	gulp.src(["dist/ps-ui.min.js", "dist/ps-template.js"])
+	gulp.src(["dist/ps-ui.min.js", "dist/ps-template-cache.min.js"])
 		.pipe(concat("ps-ui-tpls.min.js"))
 		.pipe(gulp.dest("dist/"))
 		.pipe(livereload(lrServer));
@@ -56,5 +56,5 @@ gulp.task("default", function () {
 	gulp.watch("src/**/*.js", ["scripts"]);
 	gulp.watch("template/**/*.html", ["templates"]);
 	gulp.watch("dist/ps-ui.min.js", ["final"]);
-	gulp.watch("dist/ps-template.min.js", ["final"]);
+	gulp.watch("dist/ps-template-cache.min.js", ["final"]);
 });
